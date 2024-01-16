09ec1cdfbdac5468f3aca6b670402b956dc2a717
olegs-iljanovs <olegsiljanovs5@gmail.com>
2024-01-16 12:26:19 +0200

    Header component added and images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
     <div>
       Home Page
+      <div className="absolute -z inset-0">
+      <Image 
+        src="/home.jpg"
+        alt="car factory"
+        fill
+        style={{objectFit: 'cover'}}
+      />
+      </div>
     </div>
   );
 }
